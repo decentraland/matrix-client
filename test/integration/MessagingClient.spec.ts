fc174fe57f1a31fedaeef195eef93e5055ab0424
Julieta11 <millochjuli@gmail.com>
2022-10-05 17:36:15 -0300

    test: add test case

--- test/integration/MessagingClient.spec.ts
+++ test/integration/MessagingClient.spec.ts
@@ -364,6 +364,30 @@ describe('Integration - Messaging Client', () => {
       expect(pagination2.nextBatch).to.be.undefined
     })
 
+    //
+    it ('Listen for events related to the membership updates a room receives during the sync', async () => {
+      const client = await testEnv.getRandomClient()
+
+      // Prepare spy
+      const spy = sinon.spy()
+
+      // Set listener
+      client.onChannelMembership(spy)
+
+      // Create a conversation
+      await client.joinChannel('channel-name')
+
+      // Wait for sync
+      await sleep('1s')
+
+      // Make sure that the spy was called
+      expect(spy).to.have.been.calledOnce
+
+      // Assert that the event received was sent by the actual sender
+      const sender = spy.firstCall.args[0]
+      expect(sender).to.equal(client.getUserId())
+    })
+
     /** Assert that the message was received, and return the message id */
     function assertMessageWasReceivedByEvent(
         spy,
